f55e9379a196e940f8943fe38016d51b80bb39fd
yacineb <ybsoft2011@live.com>
2019-03-06 23:28:44 +0100

    multisession debug

---
+++ src/server2.ts
@@ -0,0 +1,10 @@
+import "reflect-metadata"; // this shim is required for types reflection in runtime
+import { createExpressServer } from "routing-controllers";
+
+// creates express app, registers all controller routes and returns you express app instance
+const app = createExpressServer({
+   controllers: [__dirname + "/controllers/*Controller.js"] // import all of the controllers matching this regex
+});
+
+// run express application on port 3000
+app.listen(4000);
